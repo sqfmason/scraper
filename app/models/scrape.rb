84c8463cbe4a2b5ac5dcc6c340ab966bce6bceef
Saad <sqfmason@yahoo.com>
2015-11-13 09:44:24 -0600

    scraper is functioning well

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,28 +1,31 @@
 class Scrape
 
    	attr_accessor :title, :hotness, :image_url, :rating, :director, :genre, :release_date, :runtime, :synopsis, :failure
-
-   	def scrape_new_movie(url)
-   		begin 
-   			doc = Nokogiri::HTML(open(url))
-
-   			doc.css('script').remove
-   			self.title = doc.at("//h1[@itemprop = 'name']").text
-   			self.hotness = doc.at("span[@itemprop = 'ratingvalue']").text.to_i
-   			self.image_url = doc.at_css('#movie-image-section img')['src']
-   			self.rating = doc.at("td[@itemprop = 'contentRating']").text
-   			self.director = doc.at("//span[@itemprop = 'name']").text
-   			self.genre = doc.at("//span[@itemprop = 'genre']").text
-   			self.release_date = doc.at("//td[@itemprop = 'datePublished']").text.to_date
-   			self.runtime = doc.at("//time[@itemprop = 'duration']").text
-   			s = doc.css("#movieSynopsis").text
-   			if ! s.valid_encoding?
-   				s = s.encode("UTF-16be", :invalid=> :replace, :replace=>"?").encode('UTF-8')
-   			end
-   			return true
-   		rescue Exception => e
-   			self.failure = "Something went wrong with the scrape"
-   		end
-   	end
+def scrape_new_movie(url)
+         begin
+            doc = Nokogiri::HTML(open(url))
+            doc.css('script').remove 
+            self.title = doc.at("//h1[@itemprop = 'name']").text  
+            self.hotness = doc.at("//span[@itemprop = 'ratingValue']").text.to_i 
+            self.image_url = doc.at_css('#movie-image-section img')['src'] 
+            self.rating = doc.at("//td[@itemprop = 'contentRating']").text 
+            self.director = doc.at("//td[@itemprop = 'director']").css('a').first.text 
+            self.genre = doc.at("//span[@itemprop = 'genre']").text 
+            self.runtime = doc.at("//time[@itemprop = 'duration']").text 
+            s = doc.css('#movieSynopsis').text
+            if ! s.valid_encoding?
+               s = s.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
+            end
+            self.synopsis = s
+            return true
+         rescue Exception => e
+            self.failure = "Something went wrong with the scrape"
+         
+end
+      end
 
 end
+
+
+
+
